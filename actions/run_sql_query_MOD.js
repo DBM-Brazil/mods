7a34b86d177ecec497ed90babf9650ee0f1dba28
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-06-07 14:19:53 -0300

    Update run_sql_query_MOD.js

--- actions/run_sql_query_MOD.js
+++ actions/run_sql_query_MOD.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Run SQL Query',
   section: 'Other Stuff',
   meta: {
-    version: '2.1.3',
+    version: '2.1.4',
     preciseCheck: false,
     author: '[XinXyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Brazil/mods',
