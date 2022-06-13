8a925ecfa5018a43b9b252dd9c0a9b7f50d0391c
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-06-13 02:01:20 -0300

    Add files via upload

--- actions/set_member_channel_multi_perms_MOD.js
+++ actions/set_member_channel_multi_perms_MOD.js
@@ -15,7 +15,7 @@ module.exports = {
     return `${presets.getChannelText(data.channel, data.varName)}`;
   },
 
-  fields: ["channel", "varName", "member", "varName2", "reparticao", "state1", "state2","state3","state4","state5","state6","state7","state8","state9","state10","state11","state12","state13","state14","state15","state16","state17","state18","state19","state1v", "state2v","state3v","state4v","state5v","state6v","state7v","state8v","state9v","state10v","state11v","state12v","state13v","state14v","state15v","state16v","state17v","state18v","state19v", "reason"],
+  fields: ["channel", "varName", "member", "varName2", "reparticao", "state1", "state2","state3","state4","state5","state6","state7","state8","state9","state10","state11","state12","state13","state14","state15","state16","state17","state18","state19","state20","state21","state22","state23","state24","state25","state26","state27","state1v", "state2v","state3v","state4v","state5v","state6v","state7v","state8v","state9v","state10v","state11v","state12v","state13v","state14v","state15v","state16v","state17v","state18v","state19v","state20v","state21v","state22v","state23v","state24v","state25v","state26v","state27v", "reason"],
 
 
   html(isEvent, data) {
@@ -155,6 +155,53 @@ module.exports = {
 <option value="2">Não permitir</option>
 </select></td></tr>
 
+<tr><td>[Canal de Voz] Conectar</td><td><select id="state20" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
+
+<tr><td>[Canal de Voz] Falar</td><td><select id="state21" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
+
+<tr><td>[Canal de Voz] Video</td><td><select id="state22" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
+
+<tr><td>[Canal de Voz] Usar detecção de voz</td><td><select id="state23" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
+
+<tr><td>[Canal de Voz] Voz Prioritária</td><td><select id="state24" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
+
+<tr><td>[Canal de Voz] SIlenciar membros</td><td><select id="state25" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
+
+<tr><td>[Canal de Voz] Ensurdercer membros</td><td><select id="state26" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
+
+<tr><td>[Canal de Voz] Mover membros</td><td><select id="state27" class="round">
+<option value="0">Permitir</option>
+<option value="1" selected>Herdar</option>
+<option value="2">Não permitir</option>
+</select></td></tr>
 
 </table></center></div>
 <div id="containerdoxin2">
@@ -201,6 +248,23 @@ Use 0 para Permitir, 1 para Herdar e 2 para Não permitir<br><br>
 
 <tr><td>Usar comandos de aplicativos</td><td><input id="state19v" class="round" type="text"></td></tr>
 
+<tr><td>[Canal de voz] Conectar</td><td><input id="state20v" class="round" type="text"></td></tr>
+
+<tr><td>[Canal de Voz] Falar</td><td><input id="state21v" class="round" type="text"></td></tr>
+
+<tr><td>[Canal de Voz] Video</td><td><input id="state22v" class="round" type="text"></td></tr>
+
+<tr><td>[Canal de Voz] Usar detecção de voz</td><td><input id="state23" class="round" type="text"></td></tr>
+
+<tr><td>[Canal de Voz] Voz Prioritária</td><td><input id="state24v" class="round" type="text"></td></tr>
+
+<tr><td>[Canal de Voz] SIlenciar membros</td><td><input id="state25v" class="round" type="text"></td></tr>
+
+<tr><td>[Canal de Voz] Ensurdercer membros</td><td><input id="state26v" class="round" type="text"></td></tr>
+
+<tr><td>[Canal de Voz] Mover membros</td><td><input id="state27v" class="round" type="text"></td></tr>
+
+
 </table></center>
 </div>
 <br>
@@ -245,7 +309,7 @@ td{padding:5px;border:1px solid #777}</style>`;
     const member = await this.getMemberFromData(data.member, data.varName2, cache);
     const reason = this.evalMessage(data.reason, cache);
 
-    const options = { VIEW_CHANNEL: [true, null, false][parseInt(data.state1, 10)],MANAGE_CHANNELS: [true, null, false][parseInt(data.state2, 10)],MANAGE_WEBHOOKS: [true, null, false][parseInt(data.state3, 10)],CREATE_INSTANT_INVITE: [true, null, false][parseInt(data.state4, 10)], SEND_MESSAGES: [true, null, false][parseInt(data.state5, 10)], SEND_MESSAGES_IN_THREADS: [true, null, false][parseInt(data.state6, 10)], CREATE_PUBLIC_THREADS: [true, null, false][parseInt(data.state7, 10)], CREATE_PRIVATE_THREADS: [true, null, false][parseInt(data.state8, 10)], EMBED_LINKS: [true, null, false][parseInt(data.state9, 10)], ATTACH_FILES: [true, null, false][parseInt(data.state10, 10)], ADD_REACTIONS: [true, null, false][parseInt(data.state11, 10)], USE_EXTERNAL_EMOJIS: [true, null, false][parseInt(data.state12, 10)], USE_EXTERNAL_STICKERS: [true, null, false][parseInt(data.state13, 10)], MENTION_EVERYONE: [true, null, false][parseInt(data.state14, 10)], MANAGE_MESSAGES: [true, null, false][parseInt(data.state15, 10)], MANAGE_THREADS: [true, null, false][parseInt(data.state16, 10)], READ_MESSAGE_HISTORY: [true, null, false][parseInt(data.state17, 10)], SEND_TTS_MESSAGES: [true, null, false][parseInt(data.state18, 10)], USE_APPLICATION_COMMANDS: [true, null, false][parseInt(data.state19, 10)] };
+    const options = { VIEW_CHANNEL: [true, null, false][parseInt(data.state1, 10)],MANAGE_CHANNELS: [true, null, false][parseInt(data.state2, 10)],MANAGE_WEBHOOKS: [true, null, false][parseInt(data.state3, 10)],CREATE_INSTANT_INVITE: [true, null, false][parseInt(data.state4, 10)], SEND_MESSAGES: [true, null, false][parseInt(data.state5, 10)], SEND_MESSAGES_IN_THREADS: [true, null, false][parseInt(data.state6, 10)], CREATE_PUBLIC_THREADS: [true, null, false][parseInt(data.state7, 10)], CREATE_PRIVATE_THREADS: [true, null, false][parseInt(data.state8, 10)], EMBED_LINKS: [true, null, false][parseInt(data.state9, 10)], ATTACH_FILES: [true, null, false][parseInt(data.state10, 10)], ADD_REACTIONS: [true, null, false][parseInt(data.state11, 10)], USE_EXTERNAL_EMOJIS: [true, null, false][parseInt(data.state12, 10)], USE_EXTERNAL_STICKERS: [true, null, false][parseInt(data.state13, 10)], MENTION_EVERYONE: [true, null, false][parseInt(data.state14, 10)], MANAGE_MESSAGES: [true, null, false][parseInt(data.state15, 10)], MANAGE_THREADS: [true, null, false][parseInt(data.state16, 10)], READ_MESSAGE_HISTORY: [true, null, false][parseInt(data.state17, 10)], SEND_TTS_MESSAGES: [true, null, false][parseInt(data.state18, 10)], USE_APPLICATION_COMMANDS: [true, null, false][parseInt(data.state19, 10)], CONNECT: [true, null, false][parseInt(data.state20, 10)], SPEAK: [true, null, false][parseInt(data.state21, 10)], STREAM: [true, null, false][parseInt(data.state22, 10)], USE_VAD: [true, null, false][parseInt(data.state23, 10)], PRIORITY_SPEAKER: [true, null, false][parseInt(data.state24, 10)], MUTE_MEMBERS: [true, null, false][parseInt(data.state25, 10)], DEAFEN_MEMBERS: [true, null, false][parseInt(data.state26, 10)], MOVE_MEMBERS: [true, null, false][parseInt(data.state27, 10)] };
     let state1v = this.evalMessage(data.state1v, cache);
     let state2v = this.evalMessage(data.state2v, cache);
     let state3v = this.evalMessage(data.state3v, cache);
@@ -265,7 +329,15 @@ td{padding:5px;border:1px solid #777}</style>`;
     let state17v = this.evalMessage(data.state17v, cache);
     let state18v = this.evalMessage(data.state18v, cache);
     let state19v = this.evalMessage(data.state19v, cache);
-    const options2 = { VIEW_CHANNEL: [true, null, false][parseInt(state1v, 10)],MANAGE_CHANNELS: [true, null, false][parseInt(state2v, 10)],MANAGE_WEBHOOKS: [true, null, false][parseInt(state3v, 10)],CREATE_INSTANT_INVITE: [true, null, false][parseInt(state4v, 10)], SEND_MESSAGES: [true, null, false][parseInt(state5v, 10)], SEND_MESSAGES_IN_THREADS: [true, null, false][parseInt(state6v, 10)], CREATE_PUBLIC_THREADS: [true, null, false][parseInt(state7v, 10)], CREATE_PRIVATE_THREADS: [true, null, false][parseInt(state8v, 10)], EMBED_LINKS: [true, null, false][parseInt(state9v, 10)], ATTACH_FILES: [true, null, false][parseInt(state10v, 10)], ADD_REACTIONS: [true, null, false][parseInt(state11v, 10)], USE_EXTERNAL_EMOJIS: [true, null, false][parseInt(state12v, 10)], USE_EXTERNAL_STICKERS: [true, null, false][parseInt(state13v, 10)], MENTION_EVERYONE: [true, null, false][parseInt(state14v, 10)], MANAGE_MESSAGES: [true, null, false][parseInt(state15v, 10)], MANAGE_THREADS: [true, null, false][parseInt(state16v, 10)], READ_MESSAGE_HISTORY: [true, null, false][parseInt(state17v, 10)], SEND_TTS_MESSAGES: [true, null, false][parseInt(state18v, 10)], USE_APPLICATION_COMMANDS: [true, null, false][parseInt(state19v, 10)] };
+    let state20v = this.evalMessage(data.state20v, cache);
+    let state21v = this.evalMessage(data.state21v, cache);
+    let state22v = this.evalMessage(data.state22v, cache);
+    let state23v = this.evalMessage(data.state23v, cache);
+    let state24v = this.evalMessage(data.state24v, cache);
+    let state25v = this.evalMessage(data.state24v, cache);
+    let state26v = this.evalMessage(data.state24v, cache);
+    let state27v = this.evalMessage(data.state24v, cache);
+    const options2 = { VIEW_CHANNEL: [true, null, false][parseInt(state1v, 10)],MANAGE_CHANNELS: [true, null, false][parseInt(state2v, 10)],MANAGE_WEBHOOKS: [true, null, false][parseInt(state3v, 10)],CREATE_INSTANT_INVITE: [true, null, false][parseInt(state4v, 10)], SEND_MESSAGES: [true, null, false][parseInt(state5v, 10)], SEND_MESSAGES_IN_THREADS: [true, null, false][parseInt(state6v, 10)], CREATE_PUBLIC_THREADS: [true, null, false][parseInt(state7v, 10)], CREATE_PRIVATE_THREADS: [true, null, false][parseInt(state8v, 10)], EMBED_LINKS: [true, null, false][parseInt(state9v, 10)], ATTACH_FILES: [true, null, false][parseInt(state10v, 10)], ADD_REACTIONS: [true, null, false][parseInt(state11v, 10)], USE_EXTERNAL_EMOJIS: [true, null, false][parseInt(state12v, 10)], USE_EXTERNAL_STICKERS: [true, null, false][parseInt(state13v, 10)], MENTION_EVERYONE: [true, null, false][parseInt(state14v, 10)], MANAGE_MESSAGES: [true, null, false][parseInt(state15v, 10)], MANAGE_THREADS: [true, null, false][parseInt(state16v, 10)], READ_MESSAGE_HISTORY: [true, null, false][parseInt(state17v, 10)], SEND_TTS_MESSAGES: [true, null, false][parseInt(state18v, 10)], USE_APPLICATION_COMMANDS: [true, null, false][parseInt(state19v, 10)], CONNECT: [true, null, false][parseInt(state20v, 10)], SPEAK: [true, null, false][parseInt(state21v, 10)], STREAM: [true, null, false][parseInt(state22v, 10)], USE_VAD: [true, null, false][parseInt(state23v, 10)], PRIORITY_SPEAKER: [true, null, false][parseInt(state24v, 10)], MUTE_MEMBERS: [true, null, false][parseInt(state25v, 10)], DEAFEN_MEMBERS: [true, null, false][parseInt(state26v, 10)], MOVE_MEMBERS: [true, null, false][parseInt(state27v, 10)] };
 
     const reparticao = parseInt(data.reparticao, 10);
     
