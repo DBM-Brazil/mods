4210c1991e630f3e4e6b09c86ec53c228fc1d144
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-06-20 23:53:32 -0300

    Update check_if_member_MOD.js

--- actions/check_if_member_MOD.js
+++ actions/check_if_member_MOD.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Check If Member',
   section: 'Conditions',
   meta: {
-    version: '2.1.4',
+    version: '2.1.5',
     preciseCheck: true,
     author: '[XinXyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Brazil/mods',
